68071872a615dcead6f7dab145ac4149fb55d7cf
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2019-01-17 12:27:20 +0100

    Initial Commit

---
+++ ReusableDataInput/ReusableDataInput.h
@@ -0,0 +1,19 @@
+//
+//  ReusableDataInput.h
+//  ReusableDataInput
+//
+//  Created by Oleksandr Pronin on 17.01.19.
+//  Copyright © 2019 adviqo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReusableDataInput.
+FOUNDATION_EXPORT double ReusableDataInputVersionNumber;
+
+//! Project version string for ReusableDataInput.
+FOUNDATION_EXPORT const unsigned char ReusableDataInputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReusableDataInput/PublicHeader.h>
+
+
